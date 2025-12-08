75095a1a914fb84e3a4696e89ced86d06167e55e
Moumen Soliman <moumensoliman@gmail.com>
2025-12-08 13:36:38 +0200

    Rename middleware to proxy and change response
    Renamed middleware function to proxy and updated response behavior.

--- app/proxy.ts
+++ app/proxy.ts
@@ -4,11 +4,11 @@ import type { NextRequest } from "next/server";
 import { captureRegistryEvent } from "@wandry/analytics-sdk";
 
 // Add token to .env file
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   captureRegistryEvent(
     request,
     process.env.NEXT_PUBLIC_WANDRY_REGISTRY_TOKEN || ""
   );
 
-  return NextResponse.redirect(new URL("/", request.url));
+  return NextResponse.next();
 }
